dde55a811858ef77bd65ffc7a8846382de12cbf6
Stanislav <kolobaevs21@gmail.coml>
2025-01-02 18:12:47 +0200

    changed

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,7 @@
 function calcAverageCalories(days) {
+  if (!Array.isArray(days) || days.length === 0) {
+    return 0;
+  }
   const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
   return totalCalories / days.length;
 }
